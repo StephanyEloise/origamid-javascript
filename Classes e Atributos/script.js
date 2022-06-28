aa8d4dc2d26f15a8c131954d76814931df56dbc4
Stephany Eloise <stephany.eloise@hotmail.com>
2022-06-28 10:21:54 -0300

    Exercicio

--- Classes e Atributos/script.js
+++ Classes e Atributos/script.js
@@ -128,6 +128,10 @@ rect.height; // height do elemento
 rect.width; // width do elemento 
 rect.top; // distância entre o topo do elemento e o scroll 
 
+if(h2rect.top < 0) {
+  console.log('Passou do elemento');
+};
+
 // Window 
 
 window.innerWidth; // width da janela 
@@ -142,4 +146,66 @@ if(window.innerWidth < 600) {
   console.log('Tela menor que 600px'); 
 }; 
 
+/* matchMedia(); 
+Utilize um media-querie como no CS para verificar a largura do browser */ 
 
+const small = window.matchMedia('(max-width: 600px)'); 
+
+if(small.matches) {
+  console.log('Tela menor que 600px');
+} else { 
+  console.log('Tela maior que 600px');
+}; 
+
+/* Dicas
+Selecione o elemento no inspetor (dom)
+Abrao console e digite $0 para selecionar o mesmo 
+Os elementos selecionados anteriormente são $1, $2 ... */ 
+
+// Exercício 
+
+// Verifique a distância da primeira imagem em relação ao topo da página 
+
+const img1 = document.querySelector('img');
+const imgTop = img.offsetTop; 
+
+console.log(imgTop);
+
+// Retorne a soma da largura de todas as imagens 
+
+window.onload = function () { // Propriedade que atribue função que só é ativada quando o onload ocorre 
+  somaImagens(); 
+};
+
+const img2 = document.querySelectorAll('img'); 
+
+function somaImagens() {
+  let soma = 0; 
+  img2.forEach((imagem) => {
+    soma = soma + img2.offsetWidth; // soma += img2.offsetWidth;
+  });
+  console.log(soma);
+} ;
+
+// Verifique se os links da página possuem o mínimo recomendado para telas utilizadas com o dedo. (48px/48px de acordo com o google)
+
+const links = document.querySelectorAll('a');
+
+links.forEach((link) => {
+  const linkWidth = link.offsetWidth;
+  const linkHeight = link.offsetHeight;
+  if(linkWidth >= 48 && linkHeight >= 48) {
+    console.log(link, 'Possui boa acessibilidade');
+  } else { 
+    console.log(link, 'Não possui boa acessibilidade');
+  };
+});
+
+// Se o browser for menor que 720px, adicione a classe menu-mobile ao menu 
+
+const browserSmall = window.matchMedia('(max-width: 720 px)').matches; 
+
+if(browserSmall) { 
+  const menu = document.querySelector('.menu'); 
+  menu.classList.add('menu-mobile'); 
+};
