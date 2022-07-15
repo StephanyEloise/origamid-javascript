94371eae9675831ef239a6a0142a82f6870dd2b5
Stephany Eloise <stephany.eloise@hotmail.com>
2022-07-15 16:24:03 -0300

    Accordion List

--- 02navegacao-por-tab/script.js
+++ 02navegacao-por-tab/script.js
@@ -1,18 +1,34 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li'); 
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li'); 
 
-const tabContnt = document.querySelectorAll('.js-tabcontent section'); 
+  const tabContnt = document.querySelectorAll('.js-tabcontent section'); 
 
-tabContnt[0].classList.add('ativo');
+  if(tabMenu.length && tabContnt.length) {
+    tabContnt[0].classList.add('ativo');
 
-function activeTab(index) {
-  tabContnt.forEach((section) => {
-    section.classList.remove('ativo');
-  });
-  tabContnt[index].classList.add('ativo'); 
+    function activeTab(index) {
+      tabContnt.forEach((section) => {
+        section.classList.remove('ativo');
+      });
+      tabContnt[index].classList.add('ativo'); 
+    };
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
+    });
+  };
+};
+initTabNav();
+
+const accordionList = document.querySelectorAll('.js-accordion dt'); 
+
+function activeAccordion() {
+  this.classList.add.('ativo');
+  this.nextElementSibling.classList.add('ativo'); 
 };
 
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener('click', () => {
-    activeTab(index);
-  });
+accordionList.forEach((item) => {
+  item.addEventListener('click', activeAccordion);
 });
