5c68852d6129d08790a10fac982057f94e4875a6
Stephany Eloise <stephany.eloise@hotmail.com>
2022-07-15 16:08:20 -0300

    CSS+HTML

--- 02navegacao-por-tab/script.js
+++ 02navegacao-por-tab/script.js
@@ -2,9 +2,17 @@ const tabMenu = document.querySelectorAll('.js-tabmenu li');
 
 const tabContnt = document.querySelectorAll('.js-tabcontent section'); 
 
+tabContnt[0].classList.add('ativo');
+
 function activeTab(index) {
   tabContnt.forEach((section) => {
     section.classList.remove('ativo');
-  })
+  });
   tabContnt[index].classList.add('ativo'); 
 };
+
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click', () => {
+    activeTab(index);
+  });
+});
