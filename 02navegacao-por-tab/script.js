2deafd3c5381ec36f92cfc6926bb9df6c272d458
Stephany Eloise <stephany.eloise@hotmail.com>
2022-07-17 17:13:41 -0300

    Scroll Suave

--- 02navegacao-por-tab/script.js
+++ 02navegacao-por-tab/script.js
@@ -40,5 +40,23 @@ function initAccordion() {
     });
   };
 };
-
 initAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[herf^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault(); 
+  const herf = event.currentTarget.getAttribute('href'); 
+  const section = document.querySelector(href); 
+  const topo = section.offsetTop;
+
+  window.scrollTo({
+    top: topo,
+    behavior: 'smooth',
+  });
+};
+
+linksInternos.forEach((link) => {
+  link.addEventListener
+})
+
