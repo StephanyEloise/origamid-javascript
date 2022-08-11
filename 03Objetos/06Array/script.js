1323e5877329af2c7c4b545da71b3be8d2e360b8
Stephany Eloise <stephany.eloise@hotmail.com>
2022-08-11 11:33:27 -0300

    Exercicios

--- 03Objetos/06Array/script.js
+++ 03Objetos/06Array/script.js
@@ -128,3 +128,95 @@ const transporte2 = ['Carro', 'Moto'];
 const transporte3 = transporte1.concat(transporte2); // ['Barco', 'Aviao', 'Carro', 'Moto'];
 
 const maisTransportes = [].concat(transporte1, transporte2, 'Van'); // ['Barco', 'Aviao', 'Carro', 'Moto', 'Van'];
+
+/* [].includes(), [].indexOf() e [].lastIndexOf()
+[].includes(valor) verifica se a array possui o valor
+e retorna true ou false. 
+[].indexOf(valor) verifica se array possui o valor e 
+retorna o index do primeiro valor na array. 
+Já o [].lastIndexOf(valor) retorna o index do último.*/
+
+const linguagens = ['html', 'css', 'js', 'php', ' python', 'js']; 
+
+linguagens.includes('css'); // true 
+linguagens.includes('ruby'); // false 
+linguagens.indexOf('python'); // 4 
+linguagens.indexOf('js'); // 2
+linguagens.lastIndexOf('js'); // 5 
+
+/* [].join()
+[].join(separador) junta todos os valores da array e 
+retorna uma string com eles. Se você passar um valor
+como parâmetro, este será utilizado durante a junção 
+de cada item da array. */ 
+
+linguagens.join(); // 'html,css,js,php,python,js'
+linguagens.join(' '); // 'html css js php phython js'
+linguagens.join('-_-'); // 'html-_-css-_-js-_-php-_-phython-_-js'
+
+let htmlString = '<h2>Título Principal</h2>'
+htmlString - htmlString.split('h2'); 
+// ['<', '>Título Principal</', '>']
+htmlString = htmlString.join('h1'); 
+// <h1>Título Principal</h1>
+
+/* [].slice()
+[].slice(inicio, final) retorna os itens da array 
+começando pelo inícios e indo até o valor de final. */
+
+linguagens.slice(3); // ['js', 'php', ' python', 'js'];
+linguagens.slice(2,3); // ['js'];
+linguagens.slice(2,4); // ['js', php];
+linguagens.slice(); // Retorna toda a array  
+
+const cloneLinguagens = linguagens.slice(); 
+
+linguagens.pop()
+
+linguagens; // ['html', 'css', 'js', 'php', ' python']; 
+cloneLinguagens; // ['html', 'css', 'js', 'php', ' python', 'js']; 
+
+// Exercícios 
+
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque uma variável 
+// Remova o último valor de comidas e coloque uma variável 
+// Adicione 'Arroz'ao final da array 
+// Adicione 'Peixe' e 'Batata' ao início da array 
+
+const primeiroValor = comidas.shift(); 
+const ultimoValor = comidas.pop(); 
+
+comidas.push('Arroz');
+comidas.unshift('Peixe', 'Batata'); 
+
+
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia']; 
+// Arrume os estudantes em ordem alfabética 
+// Inverta a orem dos estudantes 
+// Verifique se Joana faz parte dos estudantes 
+// Verifique se Juliana faz parte dos estudantes 
+
+estudantes.sort(); 
+estudantes.reverse(); 
+estudantes.includes('Joana'); // true
+estudantes.includes('Juliana'); // false
+
+
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+
+// Substitua section por ul e div com li, 
+// utilizando split e join 
+
+
+const carros = [ 'Ford', 'Fiat', 'VW', 'Honda']; 
+// Remova o último carro, mas antes de remover 
+// salve a array original em outra variável 
+
+const cloneCarros = carros.slice(); 
+
+carros.pop()
