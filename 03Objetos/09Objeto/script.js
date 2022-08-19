21ac684300c4d323db0219461e2193e82261a3a0
Stephany Eloise <stephany.eloise@hotmail.com>
2022-08-19 11:00:50 -0300

    Exercici Obj

--- 03Objetos/09Objeto/script.js
+++ 03Objetos/09Objeto/script.js
@@ -1,4 +1,4 @@
-/*OBJECT
+/* OBJECT
 Todo objeto é criado com o construtor Object e por isso herda as propriedades e métodos do seu prototype.*/ 
 
 const carro1 = {
@@ -12,7 +12,7 @@ const pessoa = new Object({
 });
 
 
-/*MÉTODOS DE OBJECT
+/* MÉTODOS DE OBJECT
 Object.create(obj, defineProperties) retorna um novo objeto que terá como protótipo o objeto do primeiro argumento.*/ 
 
 const carro2 = {
@@ -32,7 +32,7 @@ const carro2 = {
 const honda = Object.create(carro2);
 honda.init('Honda').acelerar();
 
-/*OBJECT.ASSIGN()
+/* OBJECT.ASSIGN()
 Object.assign(alvo, obj1, obj2) adiciona ao alvo as propriedades e métodos enumeráveis dos demais objetos. O assign irá modificar o objeto alvo.*/
 
 const funcaoAutomovel = {
@@ -57,7 +57,7 @@ const carro3 = {
 Object.assign(moto1, funcaoAutomovel);
 Object.assign(carro3, funcaoAutomovel);
 
-/*OBJECT.DEFINEPROPERTIES()
+/* OBJECT.DEFINEPROPERTIES()
 Object.defineProperties(alvo, propriedades) adiciona ao alvo novas propriedades. A diferença aqui é a possibilidade de serem definidas as características dessas propriedades.*/ 
 
 const moto2 = {}
@@ -100,7 +100,7 @@ moto.velocidade = 200;
 moto.velocidade;
 // Velocidade 200
 
-/*OBJECT.GETOWNPROPERTYDESCRIPTORS(OBJ)
+/* OBJECT.GETOWNPROPERTYDESCRIPTORS(OBJ)
 Lista todos os métodos e propriedades de um objeto, com as suas devidas configurações.*/ 
 
 Object.getOwnPropertyDescriptors(Array);
@@ -111,3 +111,133 @@ Object.getOwnPropertyDescriptors(Array.prototype);
 
 Object.getOwnPropertyDescriptor(window, 'innerHeight');
 // Puxa de uma única propriedade
+
+/* OBJECT.KEYS(OBJ), OBJECT.VALUES(OBJ) OBJECT.ENTRIES(OBJ)
+Object.keys(obj) retorna uma array com as chaves de todas as propriedades diretas e enumeráveis do objeto. Object.values(obj) retorna uma array com os valores do objeto. Object.entries(obj) retorna uma array com array's contendo a chave e o valor. */
+
+Object.keys(Array);
+// [] vazia, pois não possui propriedades enumeráveis
+
+const carro4 = {
+  marca: 'Ford',
+  ano: 2018,
+}
+Object.keys(carro4);
+// ['marca', 'ano']
+Object.values(carro4);
+// ['Ford', 2018]
+Object.entries(carro4);
+// [['marca', 'Ford'], ['ano', 2018]]
+
+/* OBJECT.GETOWNPROPERTYNAMES(OBJ)
+Retorna uma array com todas as propriedades diretas do objeto (não retorna as do protótipo). */ 
+
+Object.getOwnPropertyNames(Array);
+// ['length', 'name', 'prototype', 'isArray', 'from', 'of']
+
+Object.getOwnPropertyNames(Array.prototype);
+// [..., 'filter', 'map', 'every', 'some', 'reduce', ...]
+
+const carro5 = {
+  marca: 'Ford',
+  ano: 2018,
+}
+Object.getOwnPropertyNames(carro5);
+// ['marca', 'ano']
+
+/* OBJECT.GETPROTOTYPEOF() E OBJECT.IS()
+Object.getPrototypeOf(), retorna o protótipo do objeto. Object.is(obj1, obj2) verifica se os objetos são iguais e retorna true ou false. */ 
+
+const frutas = ['Banana', 'Pêra']
+Object.getPrototypeOf(frutas);
+Object.getPrototypeOf(''); // String
+
+const frutas1 = ['Banana', 'Pêra'];
+const frutas2 = ['Banana', 'Pêra'];
+
+Object.is(frutas1, frutas2); // false
+
+/* OBJECT.FREEZE(), OBJECT.SEAL(), OBJECT.PREVENTEXTENSIONS()
+Object.freeze() impede qualquer mudança nas propriedades. Object.seal() previne a adição de novas propriedades e impede que as atuais sejam deletadas. Object.preventExtensions() previne a adição de novas propriedades.*/ 
+
+const carro6 = {
+  marca: 'Ford',
+  ano: 2018,
+}
+Object.freeze(carro6);
+Object.seal(carro6);
+Object.preventExtensions(carro6);
+
+Object.isFrozen(carro6); // true
+Object.isSealed(carro6); // true
+Object.isExtensible(carro6); // true
+
+/* PROPRIEDADES E MÉTODOS DO PROTÓTIPO
+Já que tudo em JavaScript é objeto, as propriedades abaixo estão disponíveis em todos os objetos criados a partir de funções construtoras. {}.constructor retorna a função construtora do objeto. */ 
+
+const frutas3 = ['Banana', 'Uva'];
+frutas3.constructor; // Array
+
+const frase = 'Isso é uma String';
+frase.constructor; // String
+
+/* {}.HASOWNPROPERTY('PROP') E {}.PROPERTYISENUMERABLE('PROP')
+Verifica se possui a propriedade e retorna true. A propriedade deve ser direta do objeto e não do protótipo. O {}.propertyIsEnumerable() verifica se a propriedade é enumerável. */
+
+const frutas4 = ['Banana', 'Uva'];
+
+frutas4.hasOwnProperty('map'); // false
+Array.hasOwnProperty('map'); // false
+Array.prototype.hasOwnProperty('map'); // true
+
+Array.prototype.propertyIsEnumerable('map'); // false
+window.propertyIsEnumerable('innerHeight'); // true
+
+/* {}.ISPROTOTYPEOF(VALOR)
+Verifica se é o protótipo do valor passado. */
+
+const frutas5 = ['Banana', 'Uva'];
+
+Array.prototype.isPrototypeOf(frutas5); // true
+
+/* {}.TOSTRING()
+Retorna o tipo do objeto. O problema é toString() ser uma função dos protótipos de Array, String e mais. Por isso é comum utilizarmos a função direto do Object.prototype.toString.call(valor). */
+
+const frutas6 = ['Banana', 'Uva'];
+frutas6.toString(); // 'Banana,Uva'
+typeof frutas6; // object
+Object.prototype.toString.call(frutas6); // [object Array]
+
+const frase2 = 'Uma String';
+frase2.toString(); // 'Uma String'
+typeof frase2; // string
+Object.prototype.toString.call(frase2); // [object String]
+
+const carro = {marca: 'Ford'};
+carro.toString(); // [object Object]
+typeof carro; // object
+Object.prototype.toString.call(carro); // [object Object]
+
+const li = document.querySelectorAll('li');
+typeof li; // object
+Object.prototype.toString.call(li); // [object NodeList]
+
+// EXERCÍCIOS
+// Crie uma função que verifique
+// corretamente o tipo de dado
+
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
+
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: '#333'
+}
+
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
